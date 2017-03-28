208a139a35363c0e9abf8fe67e2a0ffee4bc1139
Orion Osborn <orion@MacBook-Air-22.local>
2017-03-28 07:38:17 -0600

    initial test setup and part 1 of refactor

---
+++ lib/hokey_pokey.rb
@@ -0,0 +1,38 @@
+class HokeyPokey
+  def verse(number)
+    "You put your #{side(number)}#{body_part(number)} in\n" +
+    "You put your #{side(number)}#{body_part(number)} out\n" +
+    "You put your #{side(number)}#{body_part(number)} in\n" +
+    "And you shake it all about\n" +
+    "You do the Hokey Pokey and you turn yourself around\n" +
+    "That's what it's all about!\n"
+  end
+
+  def song
+    (1..6).map { |number| verse(number) }.join("\n")
+  end
+
+  private
+
+  def side(number)
+    if number == 5 || number == 6
+      ""
+    elsif number % 2 == 1
+      "right "
+    else
+      "left "
+    end
+  end
+
+  def body_part(number)
+    if number == 1 || number == 2
+      "foot"
+    elsif number == 3 || number == 4
+      "hand"
+    elsif number == 5
+      "head"
+    elsif number == 6
+      "whole self"
+    end
+  end
+end
